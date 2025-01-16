43af2660efbaa91c8280e32ab03df6f5481da493
João Paulo <jplopapa.dev@gmail.com>
2025-01-16 13:47:07 -0300

    insert implementation

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -27,5 +28,10 @@ public class Main {
         for(Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 4: Seller Insert ===");
+        Seller newSeller = new Seller(null, "Joao", "joao@gmail.com", new Date(), 3000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new insert: " + newSeller.getId());
     }
 }
